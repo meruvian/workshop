4a7b392c1d6409ee2cbb84a3c915c472cfa68667
ludvianto.ovandi@meruvian.org <ludvianto.ovandi@meruvian.org>
2015-02-25 14:40:33 +0700

    Commit Android Sample Form

---
+++ merv-workshop-android-sample-form/sample-form/src/main/java/org/meruvian/tutorial/form/SampleFormActivity.java
@@ -0,0 +1,17 @@
+package org.meruvian.tutorial.form;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by ludviantoovandi on 25/02/15.
+ */
+public class SampleFormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
